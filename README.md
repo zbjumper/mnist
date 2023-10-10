741ef9943ee5e0bed4b363a1f278a9e52e2fb61a
张博 <zbjumper@gmail.com>
2023-10-10 14:39:30 +0800

    补充说明文档

---
+++ README.md
@@ -0,0 +1,27 @@
+# MNIST
+
+手写数字识别 demo
+
+## 模型结构
+
+1. 两层卷积层
+2. 两层池化层
+3. 两层全连接层
+4. 一层输出层
+
+## 使用方法
+
+1. 安装依赖
+
+```bash
+pip install -r requirements.txt
+```
+2. 安装 pytorch
+
+根据本机 GPU 环境，参照[pytorch官网](https://pytorch.org/get-started/locally/)方法安装
+
+3. 启动
+   
+```bash
+python main.py
+```
--- main.py
+++ main.py
@@ -4,37 +4,41 @@ import torch.utils.data as Data
 import torchvision
 
 import torch.nn.functional as F
+
+
 class CNN(nn.Module):
     def __init__(self):
-        super(CNN,self).__init__()
-        self.conv1 = nn.Conv2d(1,32,kernel_size=3,stride=1,padding=1)
-        self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(32,64,kernel_size=3,stride=1,padding=1)
-        self.fc1 = nn.Linear(64*7*7,1024)#两个池化，所以是7*7而不是14*14
-        self.fc2 = nn.Linear(1024,512)
-        self.fc3 = nn.Linear(512,10)
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64*7*7, 1024)  # 两个池化，所以是7*7而不是14*14
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 10)
 #         self.dp = nn.Dropout(p=0.5)
-    def forward(self,x):
+
+    def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-             
-        x = x.view(-1, 64 * 7* 7) # 将数据平整为一维的
+
+        x = x.view(-1, 64 * 7 * 7)  # 将数据平整为一维的
         x = F.relu(self.fc1(x))
 #         x = self.fc3(x)
 #         self.dp(x)
-        x = F.relu(self.fc2(x))   
-        x = self.fc3(x)  
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
 #         x = F.log_softmax(x,dim=1) NLLLoss()才需要，交叉熵不需要
         return x
 
+
 # 批数量
-BATCH_SIZE=32
+BATCH_SIZE = 32
 # 学习率
-LR=0.01
+LR = 0.01
 # 训练次数
-EPOCH=5
+EPOCH = 5
 # 测试数量
-CESHI=2500
+CESHI = 2500
 
 train_data = torchvision.datasets.MNIST(
     root='./data',
@@ -43,7 +47,7 @@ train_data = torchvision.datasets.MNIST(
     download=True
 )
 
-test_data= torchvision.datasets.MNIST(
+test_data = torchvision.datasets.MNIST(
     root='./data',
     train=False
 )
@@ -54,8 +58,9 @@ train_loader = Data.DataLoader(
     shuffle=True,
 )
 
-test_x=torch.unsqueeze(test_data.data, dim=1).type(torch.FloatTensor)[:CESHI]/255
-test_y=test_data.targets[:CESHI]
+test_x = torch.unsqueeze(test_data.data, dim=1).type(
+    torch.FloatTensor)[:CESHI]/255
+test_y = test_data.targets[:CESHI]
 # print('current ago yum')
 # print(train_data.data.size())
 # print(test_data.data.size())
@@ -73,7 +78,7 @@ optimizer = torch.optim.SGD(net.parameters(), lr=LR)
 loss_func = nn.CrossEntropyLoss()
 
 for epoch in range(EPOCH):
-    for step, (x,y) in enumerate(train_loader):
+    for step, (x, y) in enumerate(train_loader):
         x, y = x.to(device), y.to(device)
 
         # 计算输出
@@ -89,15 +94,16 @@ for epoch in range(EPOCH):
         optimizer.step()
 
         if step % 100 == 99:
-            test_output=net(test_x)
-            pred_y = torch.max(test_output,1)[1].data.squeeze()
-            accuracy =(pred_y == test_y).sum().item()/CESHI
-            print('step:', step + 1,'| epoch:',epoch,'| loss:',loss.data.cpu().numpy(),'| accuracy:',accuracy)
+            test_output = net(test_x)
+            pred_y = torch.max(test_output, 1)[1].data.squeeze()
+            accuracy = (pred_y == test_y).sum().item()/CESHI
+            print('step:', step + 1, '| epoch:', epoch, '| loss:',
+                  loss.data.cpu().numpy(), '| accuracy:', accuracy)
 
 test_output = net(test_x)
-print("test_output.shape:",test_output.shape)
-pred_y=torch.max(test_output,1)[1].data.squeeze()
-print('真实:',test_y.cpu().numpy())
-print('预测:',pred_y.cpu().numpy())
-accuracy =(pred_y == test_y).sum().item()/CESHI
-print('accuracy:',accuracy)
+print("test_output.shape:", test_output.shape)
+pred_y = torch.max(test_output, 1)[1].data.squeeze()
+print('真实:', test_y.cpu().numpy())
+print('预测:', pred_y.cpu().numpy())
+accuracy = (pred_y == test_y).sum().item()/CESHI
+print('accuracy:', accuracy)
